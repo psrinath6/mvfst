2a5cb26980d7e440ed518447cea4e28c63f26ed6
psrinath <90727815+psrinath6@users.noreply.github.com>
2021-11-19 13:33:46 +0530

    Throughput calculator script

---
+++ calcthroughput.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+file=throughput.txt
+sum=0
+
+if ! [ -e "$file" ] ; then
+    echo "$0: $file does not exist" >&2
+    exit 1
+fi
+
+numLines=$(wc -l clientloggers.txt | cut -d " " -f1)
+activeProcs=$(ps aux | grep tperf | wc -l)
+connErrors=$(cat clientloggers.txt | grep error | wc -l)
+
+if [ $activeProcs -gt 1 ] ; then
+        echo "The file is still being written to"
+        exit 1
+fi
+
+withoutError=$(($1 - $connErrors))
+echo "Successful connections: $withoutError"
+awk '{ sum += $1 } END { print "Total Throughput for in Mb/s: ", sum }' "$file"
