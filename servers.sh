aa9735ab19a21ae2bc58e87e1a72ebc7e4c8e411
psrinath <90727815+psrinath6@users.noreply.github.com>
2021-11-20 12:32:24 +0530

    Update servers.sh

--- servers.sh
+++ servers.sh
@@ -2,8 +2,7 @@
 
 rm serverlogs.txt
 
-for (( i=5000; i<=5045; i+=5 )); then
-do
+for (( i=5000;i<=5045;i+=5 )); do
     echo "Starting server at port $i" >> serverlogs.txt
     ./_build/build/quic/tools/tperf/tperf -mode=server -host=10.10.1.1 -port=$i -num_server_worker=1 -gso=true -use_inplace_write=true -writes_per_loop=100 >> serverlogs.txt 2>&1 &
 done
