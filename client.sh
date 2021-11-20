4f2e1b6d0410a7f7ed4489a6ca0761721f91f45c
psrinath <90727815+psrinath6@users.noreply.github.com>
2021-11-20 12:27:19 +0530

    added client transport timer

--- client.sh
+++ client.sh
@@ -4,5 +4,5 @@ rm clientloggers.txt
 rm throughput.txt
 for (( i=0;i<500;i++ )); do
         echo "Starting client $i" >> clientloggers.txt
-         ./_build/build/quic/tools/tperf/tperf -mode=client -host=10.10.1.1 -use_inplace_write=true -gso=true -port=$1 -duration=60 -window=10000000000 >> clientloggers.txt 2>&1 &
+         ./_build/build/quic/tools/tperf/tperf -mode=client -host=10.10.1.1 -use_inplace_write=true -gso=true -port=$1 -duration=60 -window=10000000000 -client_transport_timer_resolution_ms=6000 >> clientloggers.txt 2>&1 &
 done
